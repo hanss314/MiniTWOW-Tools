f31ca1b66500a18a5e3bb47f21b630af2d645083
hanss314 <hanss314@gmail.com>
2017-05-11 16:58:50 -0700

    new examples
    for updated stuff

--- exampleTwow/voteGenerator.py
+++ exampleTwow/voteGenerator.py
@@ -5,15 +5,19 @@ def main():
 	parser.add_argument("-v", "--votes", nargs='?', const=10, default=10)
 	args = parser.parse_args()
 	keys = list(json.load(open('./dict.json','r')).keys())
-	writer = open('votes.txt','w')
+	writer = open('votes.csv','w')
 	options = ['a','b','c','d','e','f','g','h','i','j']	
+	voters = range(int(int(args.votes)*9/10))
 	
 	for i in range(int(args.votes)):
+	
 		random.shuffle(options)
-		voteString = '['+random.choice(keys)+' '
-		for c in options:
-			voteString += c
-		writer.write(voteString+']\n')
+		to_show = random.choice(range(1,11))
+		voter = random.choice(voters)
+		key = random.choice(keys)
+		vote = ''.join(options[:to_show])
+		vote_str = '{},[{} {}],\n'.format(voter,key,vote)
+		writer.write(vote_str)
 	
 	
 	
